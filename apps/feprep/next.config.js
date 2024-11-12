1db4df53ce1b8d77cb147ecc0c6d67d13f217bfa
Caleb Rivera <calebrossr@gmail.com>
2024-11-12 16:26:22 -0500

    add feprep boilerplate

---
+++ apps/feprep/next.config.js
@@ -0,0 +1,25 @@
+import { fileURLToPath } from "url";
+import { createJiti } from "jiti";
+
+// Import env files to validate at build time. Use jiti so we can load .ts files in here.
+await createJiti(fileURLToPath(import.meta.url)).import("./src/env");
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: [
+    "@blade/api",
+    "@blade/auth",
+    "@blade/db",
+    "@blade/ui",
+    "@blade/validators",
+  ],
+
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
+
+export default config;
---
+++ apps/feprep/src/app/page.tsx
@@ -0,0 +1,17 @@
+import { HydrateClient } from "~/trpc/server";
+import { AuthShowcase } from "./_components/auth-showcase";
+
+export default function HomePage() {
+  return (
+    <HydrateClient>
+      <main className="container h-screen py-16">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+            FEPrep
+          </h1>
+          <AuthShowcase />
+        </div>
+      </main>
+    </HydrateClient>
+  );
+}
