4ca3285528a039d91f5ade7187e9b3f564cbcebe
Caleb Rivera <calebrossr@gmail.com>
2024-11-12 17:07:39 -0500

    add greeting and secret message procedures to appRouter

--- apps/feprep/src/server/api/root.ts
+++ apps/feprep/src/server/api/root.ts
@@ -1,11 +1,19 @@
-import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import {
+  createCallerFactory,
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  greeting: publicProcedure.query(() => "Hello, world!"),
+  getSecretMessage: protectedProcedure.query(() => "This is a secret message."),
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
