30f859a936e0378de21ac2dc964512d9a788373d
Caleb Rivera <calebrossr@gmail.com>
2024-11-19 00:36:42 -0500

    add toggle solution component

---
+++ apps/feprep/src/app/problems/[id]/_components/toggle-solution.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
+
+import { RocketIcon } from "@blade/ui";
+import { Button } from "@blade/ui/button";
+
+export function ToggleSolution() {
+  const searchParams = useSearchParams();
+  const pageNumber = Number(searchParams.get("page"));
+  const pathname = usePathname();
+
+  return (
+    <Link href={`${pathname}?page=${pageNumber === 1 ? 2 : 1}`}>
+      <Button variant="outline" className="w-9 px-0 lg:w-auto lg:px-4">
+        <RocketIcon className="lg:mr-2" />
+        <span className="hidden lg:inline">
+          {pageNumber === 1 ? "Solution" : "Question"}
+        </span>
+      </Button>
+    </Link>
+  );
+}
