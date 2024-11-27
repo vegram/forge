601d61513d5156cd09e99b9218ecc40be47001b3
Caleb Rivera <calebrossr@gmail.com>
2024-11-26 21:30:37 -0500

    add member application page

---
+++ apps/app/src/app/member/application/page.tsx
@@ -0,0 +1,19 @@
+import { redirect } from "next/navigation";
+
+import { auth } from "@blade/auth";
+
+import { MemberApplicationForm } from "./_components/member-application-form";
+
+export default async function MemberApplicationPage() {
+  const session = await auth();
+
+  if (session == null) {
+    redirect("/sign-in");
+  }
+
+  return (
+    <main className="px-4">
+      <MemberApplicationForm />
+    </main>
+  );
+}
