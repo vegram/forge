178c87b12e4ae48004c259e086bc0eb733c81636
Caleb Rivera <calebrossr@gmail.com>
2024-11-30 12:57:55 -0500

    complete membership form

--- apps/app/src/app/member/application/_components/member-application-form.tsx
+++ apps/app/src/app/member/application/_components/member-application-form.tsx
@@ -2,7 +2,13 @@
 
 import { z } from "zod";
 
-import { GENDERS, SHIRT_SIZES } from "@blade/consts/knight-hacks";
+import {
+  GENDERS,
+  LEVELS_OF_STUDY,
+  RACES_OR_ETHNICITIES,
+  SCHOOLS,
+  SHIRT_SIZES,
+} from "@blade/consts/knight-hacks";
 import { InsertMemberSchema } from "@blade/db/schemas/knight-hacks";
 import { Button } from "@blade/ui/button";
 import {
@@ -15,6 +21,7 @@ import {
   useForm,
 } from "@blade/ui/form";
 import { Input } from "@blade/ui/input";
+import { ResponsiveComboBox } from "@blade/ui/responsive-combo-box";
 import {
   Select,
   SelectContent,
@@ -26,11 +33,14 @@ import {
 export function MemberApplicationForm() {
   const form = useForm({
     schema: InsertMemberSchema.extend({
+      // userId will be derived from the user's session on the server
+      userId: z.undefined(),
       firstName: z.string().min(1, "Required"),
       lastName: z.string().min(1, "Required"),
       age: z
         .string()
         .min(1, "Required")
+        .refine((value) => !isNaN(parseInt(value)), "Invalid age")
         .transform((value) => parseInt(value))
         .refine(
           (age) => age >= 18,
@@ -41,11 +51,6 @@ export function MemberApplicationForm() {
       phoneNumber: z
         .string()
         .regex(/^\d{3}-\d{3}-\d{4}$/, "Invalid phone number"),
-      addressLine1: z.string().min(1, "Required"),
-      addressLine2: z.string().optional(),
-      city: z.string().min(1, "Required"),
-      state: z.string().min(1, "Required"),
-      zipCode: z.string().min(1, "Required"),
     }),
     defaultValues: {
       firstName: "",
@@ -53,22 +58,19 @@ export function MemberApplicationForm() {
       age: "",
       email: "",
       phoneNumber: "",
-      addressLine1: "",
-      addressLine2: "",
-      city: "",
-      state: "",
-      zipCode: "",
     },
   });
 
   return (
     <Form {...form}>
       <form
+        className="space-y-4"
         noValidate
         onSubmit={form.handleSubmit((values) => {
           console.log(values);
         })}
       >
+        <h1 className="text-2xl font-bold">Application Form</h1>
         <FormField
           control={form.control}
           name="firstName"
@@ -97,12 +99,38 @@ export function MemberApplicationForm() {
         />
         <FormField
           control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="johndoe@gmail.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="phoneNumber"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Phone Number</FormLabel>
+              <FormControl>
+                <Input type="tel" placeholder="123-456-7890" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
           name="age"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Age</FormLabel>
               <FormControl>
-                <Input type="number" placeholder="18" {...field} />
+                <Input placeholder="18" {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -139,6 +167,85 @@ export function MemberApplicationForm() {
         />
         <FormField
           control={form.control}
+          name="raceOrEthnicity"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Race or Ethnicity</FormLabel>
+              <FormControl>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select your race or ethnicity" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {RACES_OR_ETHNICITIES.map((raceOrEthnicity) => (
+                      <SelectItem key={raceOrEthnicity} value={raceOrEthnicity}>
+                        {raceOrEthnicity}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="levelOfStudy"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Level of Study</FormLabel>
+              <FormControl>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select your level of study" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {LEVELS_OF_STUDY.map((levelOfStudy) => (
+                      <SelectItem key={levelOfStudy} value={levelOfStudy}>
+                        {levelOfStudy}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="school"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>School</FormLabel>
+              <FormControl>
+                <ResponsiveComboBox
+                  items={SCHOOLS}
+                  renderItem={(school) => <div>{school}</div>}
+                  getItemValue={(school) => school}
+                  getItemLabel={(school) => school}
+                  onItemSelect={(school) => field.onChange(school)}
+                  buttonPlaceholder="Select your school"
+                  inputPlaceholder="Search for your school"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
           name="shirtSize"
           render={({ field }) => (
             <FormItem>
@@ -161,101 +268,6 @@ export function MemberApplicationForm() {
                     ))}
                   </SelectContent>
                 </Select>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="johndoe@gmail.com" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="phoneNumber"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Phone Number</FormLabel>
-              <FormControl>
-                <Input type="tel" placeholder="123-456-7890" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="addressLine1"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Address Line 1</FormLabel>
-              <FormControl>
-                <Input placeholder="4000 Central Florida Blvd" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="addressLine2"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Address Line 2</FormLabel>
-              <FormControl>
-                <Input
-                  placeholder="APT 407"
-                  {...field}
-                  value={field.value ?? ""}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="city"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>City</FormLabel>
-              <FormControl>
-                <Input placeholder="Orlando" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="state"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>State</FormLabel>
-              <FormControl>
-                <Input placeholder="Florida" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="zipCode"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Zip Code</FormLabel>
-              <FormControl>
-                <Input placeholder="32816" {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
