97fe1578fe32cd9615f839618c69dec3c59306ed
anna-sage-local <anna.macinnis109@gmail.com>
2024-12-17 08:41:47 -0500

    validate resumeUpload input is FileList type

--- apps/blade/src/app/member/application/_components/member-application-form.tsx
+++ apps/blade/src/app/member/application/_components/member-application-form.tsx
@@ -111,6 +111,8 @@ export function MemberApplicationForm() {
     },
   });
 
+  const fileRef = form.register("resumeUpload");
+
   return (
     <Form {...form}>
       <form
@@ -375,7 +377,11 @@ export function MemberApplicationForm() {
             <FormItem>
               <FormLabel>Resume</FormLabel>
               <FormControl>
-                <Input type="file" placeholder="" {...field} />
+                <Input type="file" placeholder="" {...fileRef} 
+                onChange={(event) => {
+                  field.onChange(event?.target?.files[0] ?? undefined);
+                }}
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
