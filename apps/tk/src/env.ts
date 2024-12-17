1180cafa207d0ddd964f285adfb2ba8e667bcaf2
Caleb Rivera <calebrossr@gmail.com>
2024-12-17 18:26:47 -0500

    fix environment variable references in tk

--- apps/tk/src/env.ts
+++ apps/tk/src/env.ts
@@ -14,10 +14,10 @@ export const env = createEnv({
     DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN,
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
-    DISCORD_LEETCODE_ROLE_ID: process.env.DISCORD_LEETCODE,
-    DISCORD_ANIMAL_WEBHOOK_URL: process.env.DISCORD_ANIMAL_WEBHOOK_URL,
+    DISCORD_LEETCODE_ROLE_ID: process.env.DISCORD_LEETCODE_ROLE_ID,
+    DISCORD_ANIMAL_WEBHOOK_URL: process.env.DISCORD_DAILY_ANIMAL_WEBHOOK_URL,
     DISCORD_LEETCODE_DAILY_WEBHOOK_URL:
-      process.env.DISCORD_DAILY_LEETCODE_WEBHOOK_URL,
+      process.env.DISCORD_LEETCODE_DAILY_WEBHOOK_URL,
   },
   skipValidation:
     !!process.env.CI || process.env.npm_lifecycle_event === "lint",
