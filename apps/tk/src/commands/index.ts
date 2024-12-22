d37aa2a752d173ca8be87801c3598de89fce8751
must108 <mustaeen18@gmail.com>
2024-12-22 04:27:18 -0500

    add fox command!

--- apps/tk/src/commands/index.ts
+++ apps/tk/src/commands/index.ts
@@ -2,17 +2,18 @@ import * as beep from "./beep";
 import * as bubblewrap from "./bubblewrap";
 import * as capybara from "./capybara";
 import * as cat from "./cat";
+import * as chat from "./chat";
 import * as check_points from "./check_points";
 import * as dog from "./dog";
 import * as duck from "./duck";
 import * as flex from "./flex";
 import * as flowchart from "./flowchart";
+import * as fox from "./fox";
 import * as goat from "./goat";
 import * as help from "./help";
 import * as joke from "./joke";
 import * as leaderboard from "./leaderboard";
 import * as links from "./links";
-import * as chat from "./chat";
 
 // Export all commands
 export const commands = {
@@ -26,6 +27,7 @@ export const commands = {
   duck,
   flex,
   flowchart,
+  fox,
   goat,
   help,
   joke,
