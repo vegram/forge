d9c1bd3caee116e39c542f0c0d2f96675b79402a
must108 <mustaeen18@gmail.com>
2024-12-23 19:40:40 -0500

    add admin page, corrected responsiveness in payment page

--- apps/blade/src/app/admin/page.tsx
+++ apps/blade/src/app/admin/page.tsx
@@ -1,33 +1,29 @@
 import Link from "next/link";
-import { redirect } from "next/navigation";
 
 import { auth } from "@forge/auth";
 
-import { api, HydrateClient } from "~/trpc/server";
-import { SIGN_IN_PATH } from "~/consts";
+import { HydrateClient } from "~/trpc/server";
 
 export default async function Admin() {
-  const session = await auth();
-  if (!session) {
-    redirect(SIGN_IN_PATH);
-  }
+    const session = await auth();
+    // will add admin auth if it becomes a thing
 
-  const isAdmin = await api.auth.getAdminStatus();
-  if (!isAdmin) {
-    redirect("/");
-  }
-
-  return (
-    <HydrateClient>
-      <main className="container h-screen py-16">
-        <div className="flex flex-col items-center justify-center gap-4">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Admin
-          </h1>
-          <Link href={"/admin/members"}>Member Dashboard</Link>
-          <Link href={"/admin/events"}>Event Dashboard</Link>
-        </div>
-      </main>
-    </HydrateClient>
-  );
+    return (
+        <HydrateClient>
+            <main className="container h-screen py-16">
+                <div className="flex flex-col items-center justify-center gap-4">
+                    <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+                        Admin
+                    </h1>
+                    {session ? 
+                        <>
+                            <Link href={"/admin/users"}>User Dashboard</Link>
+                            <Link href={"/admin/events"}>Event Dashboard</Link>
+                        </>
+                        : "You are not authenticated to use this service!"
+                    }
+                </div>
+            </main>
+        </HydrateClient>
+    );
 }
