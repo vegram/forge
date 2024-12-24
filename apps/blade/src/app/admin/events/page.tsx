64283f288c56f630ff68ce818f3fbb1fec0c1728
must108 <mustaeen18@gmail.com>
2024-12-24 05:00:48 -0500

    working on create-member-form + memberapp updates

--- apps/blade/src/app/admin/events/page.tsx
+++ apps/blade/src/app/admin/events/page.tsx
@@ -6,9 +6,9 @@ export default function Users() {
     return (
         <HydrateClient>
             <main className="container h-screen py-16">
-                <div className="flex flex=col items-center justify-center gap-4">
-                    <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-                        Events
+                <div className="flex flex-col items-center justify-center gap-4">
+                    <h1 className="text-5xl text-center font-extrabold tracking-tight sm:text-[5rem]">
+                        Event Dashboard
                     </h1>
                 </div>
             </main>
---
+++ apps/blade/src/app/admin/members/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "@forge/ui/button";
+
+import { api } from "~/trpc/react";
+import { DataTable } from "../_components/data-table";
+import { CreateMemberForm } from "./create-member-form";
+
+export default function Members() {
+    return (
+        <main className="container h-screen py-16">
+            <div className="flex flex-col items-center justify-center gap-4">
+                <h1 className="text-5xl text-center font-extrabold tracking-tight sm:text-[5rem]">
+                    Member Dashboard
+                </h1>
+                <CreateMemberForm />
+            </div>
+        </main>
+    )
+}
