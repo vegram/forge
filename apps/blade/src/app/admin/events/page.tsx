c71e5a99ff192035b9362f80423f5ced3018c1c7
must108 <mustaeen18@gmail.com>
2024-12-23 19:49:31 -0500

    add initial users and events page

---
+++ apps/blade/src/app/admin/events/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+import { HydrateClient } from "~/trpc/server";
+
+export default function Users() {
+    return (
+        <HydrateClient>
+            <main className="container h-screen py-16">
+                <div className="flex flex=col items-center justify-center gap-4">
+                    <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+                        Events
+                    </h1>
+                </div>
+            </main>
+        </HydrateClient>
+    )
+}
