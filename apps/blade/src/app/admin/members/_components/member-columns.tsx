5575181c7a85c3d706be144c9f4fbbbc17ed8a91
must108 <mustaeen18@gmail.com>
2024-12-24 20:33:14 -0500

    add sheet.tsx

--- apps/blade/src/app/admin/members/_components/member-columns.tsx
+++ apps/blade/src/app/admin/members/_components/member-columns.tsx
@@ -4,5 +4,15 @@ import { toast } from "@forge/ui/toast";
 import type { RouterOutputs } from "@forge/api";
 import type { ColumnDef, Row } from "@tanstack/react-table";
 import { CaretSortIcon, DotsHorizontalIcon } from "@forge/ui";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "@forge/ui/dropdown-menu";
 
 import { api } from "~/trpc/react";
+
+
--- packages/ui/src/table.tsx
+++ packages/ui/src/table.tsx
@@ -58,7 +58,8 @@ const TableRow = React.forwardRef<
     <tr
         ref={ref}
         className={cn(
-            "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+            "border-b transition-colors hover:bg-muted/50",
+            "data-[state=selected]:bg-muted",
             className,
         )}
         {...props}
@@ -73,7 +74,8 @@ const TableHead = React.forwardRef<
     <th
         ref={ref}
         className={cn(
-            "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+            "h-12 px-4 text-left align-middle font-medium",
+            "text-muted-foreground [&:has([role=checkbox])]:pr-0",
             className,
         )}
         {...props}
@@ -87,7 +89,8 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
     <td
         ref={ref}
-        className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
+        className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0",
+            className)}
         {...props}
     />
 ));
