3ba3b984637b896338a803be16a559efdb8bc682
must108 <136145181+must108@users.noreply.github.com>
2024-12-25 23:04:23 -0500

    secured /admin and /admin/members

--- apps/blade/src/app/admin/members/page.tsx
+++ apps/blade/src/app/admin/members/page.tsx
@@ -1,13 +1,32 @@
-"use client";
+import { redirect } from "next/navigation";
 
-export default function Members() {
+import { auth } from "@forge/auth";
+
+import { isDiscordAdmin } from "~/app/utils";
+import { SIGN_IN_PATH } from "~/consts";
+import { HydrateClient } from "~/trpc/server";
+
+export default async function Members() {
+  // authentication
+  const session = await auth();
+  if (!session) {
+    redirect(SIGN_IN_PATH);
+  }
+
+  const isAdmin = await isDiscordAdmin(session.user);
+  if (!isAdmin) {
+    redirect("/");
+  }
+
   return (
-    <main className="container h-screen py-16">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-center text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Member Dashboard
-        </h1>
-      </div>
-    </main>
+    <HydrateClient>
+      <main className="container h-screen py-16">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="text-center text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+            Member Dashboard
+          </h1>
+        </div>
+      </main>
+    </HydrateClient>
   );
 }
