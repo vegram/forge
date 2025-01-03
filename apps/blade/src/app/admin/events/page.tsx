ece82b0fbbc672346b2e908435a539a1614b0123
Dylan Vidal <dylanvidal1204@gmail.com>
2025-01-03 16:04:11 -0500

    remove initial sandbox

--- apps/blade/src/app/admin/events/page.tsx
+++ apps/blade/src/app/admin/events/page.tsx
@@ -1,9 +1,6 @@
 import { redirect } from "next/navigation";
-import { and, gte, lt } from "drizzle-orm/expressions";
 
 import { auth } from "@forge/auth";
-import { db } from "@forge/db/client";
-import { Event } from "@forge/db/schemas/knight-hacks";
 
 import { SIGN_IN_PATH } from "~/consts";
 import { api, HydrateClient } from "~/trpc/server";
@@ -15,20 +12,6 @@ export default async function Events() {
   if (!session) {
     redirect(SIGN_IN_PATH);
   }
-
-  // Grab all events occurring in the next 7 days
-  const today = new Date();
-  const nextWeek = new Date(today);
-  nextWeek.setDate(today.getDate() + 7);
-
-  const events = await db
-    .select()
-    .from(Event)
-    .where(
-      and(gte(Event.start_datetime, today), lt(Event.start_datetime, nextWeek)),
-    );
-
-  console.log(events);
 
   // Check if the user has access to Blade
   const isAdmin = await api.auth.getAdminStatus();
