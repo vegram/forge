bb0a0aae8d318fcf16201e7e3166ce5e449eccf5
Dylan Vidal <dylanvidal1204@gmail.com>
2025-01-04 16:06:53 -0500

    add root url quick link

--- apps/blade/src/app/_components/navigation/session-navbar.tsx
+++ apps/blade/src/app/_components/navigation/session-navbar.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import {
   NavigationMenu,
@@ -15,16 +16,18 @@ export async function SessionNavbar() {
 
   return (
     <div className="flex items-center justify-between px-3 py-3 sm:px-10 sm:py-5">
-      <div className="flex items-center justify-center gap-x-2 text-lg font-extrabold sm:text-[2rem]">
-        <Image
-          src={"/white-kh-logo.svg"}
-          alt="The logo of Knight Hacks"
-          width={0}
-          height={0}
-          style={{ width: "35px", height: "35px" }}
-        />
-        Blade
-      </div>
+      <Link href="/">
+        <div className="flex items-center justify-center gap-x-2 text-lg font-extrabold sm:text-[2rem]">
+          <Image
+            src={"/white-kh-logo.svg"}
+            alt="The logo of Knight Hacks"
+            width={0}
+            height={0}
+            style={{ width: "35px", height: "35px" }}
+          />
+          Blade
+        </div>
+      </Link>
       <Separator className="absolute left-0 top-16 sm:top-20" />
       <NavigationMenu className="h-[35px] w-[35px]">
         <NavigationMenuList>
