92db70be789481b4056dbba19c381ba2c4973ea2
Dylan Vidal <dylanvidal1204@gmail.com>
2025-01-04 15:46:15 -0500

    update tabs (TODO when hackers implemnted)

--- apps/blade/src/app/_components/user-interface.tsx
+++ apps/blade/src/app/_components/user-interface.tsx
@@ -1,7 +1,5 @@
 import { redirect } from "next/navigation";
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@forge/ui/tabs";
-
 import { api } from "~/trpc/server";
 import MemberDashboard from "./member-dashboard/member-dashboard";
 
@@ -13,29 +11,36 @@ export async function UserInterface() {
     redirect("/member/application");
   }
 
+  // TODO: Completely removing the tabs for now, until we implement Hacker logic.
+  // We want the tabs only to show when there are multiple profiles, and since there is only one implemented, we will return just that
+
   return (
-    <div className="flex justify-center">
-      <Tabs defaultValue="Member" className="w-[400px]">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger
-            value="Member"
-            className="data-[state=active]:bg-primary data-[state=active]:text-white"
-          >
-            Member
-          </TabsTrigger>
-          <TabsTrigger
-            value="Hacker"
-            className="data-[state=active]:bg-primary data-[state=active]:text-white"
-            disabled
-          >
-            Hacker (soon)
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent className="absolute left-0 w-full" value="Member">
-          <MemberDashboard member={member} />
-        </TabsContent>
-        <TabsContent value="Hacker"></TabsContent>
-      </Tabs>
+    <div className="absolute left-0 w-full">
+      <MemberDashboard member={member} />
     </div>
   );
+
+  // return (
+  //   <div className="flex justify-center">
+  //     <Tabs defaultValue="Member" className="w-[400px]">
+  //       <TabsList className="grid w-full grid-cols-2">
+  //         <TabsTrigger
+  //           value="Member"
+  //           className="data-[state=active]:bg-primary data-[state=active]:text-white"
+  //         >
+  //           Member
+  //         </TabsTrigger>
+  //         <TabsTrigger
+  //           value="Hacker"
+  //           className="data-[state=active]:bg-primary data-[state=active]:text-white"
+  //         >
+  //           Hacker (soon)
+  //         </TabsTrigger>
+  //       </TabsList>
+  //       <TabsContent className="absolute left-0 w-full" value="Member">
+  //         <MemberDashboard member={member} />
+  //       </TabsContent>
+  //       <TabsContent value="Hacker"></TabsContent>
+  //     </Tabs>
+  //   </div>
 }
