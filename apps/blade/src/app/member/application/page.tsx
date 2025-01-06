8e92356328d2fbc04a0d7dbd5448123834987f7e
must108 <136145181+must108@users.noreply.github.com>
2025-01-06 11:55:09 -0500

    fixes

--- apps/blade/src/app/member/application/page.tsx
+++ apps/blade/src/app/member/application/page.tsx
@@ -13,11 +13,11 @@ export default async function MemberApplicationPage() {
     redirect(SIGN_IN_PATH);
   }
 
-  // const isMember = await api.member.getMember();
+  const isMember = await api.member.getMember();
 
-  // if (isMember) {
-  //   return redirect(SIGN_IN_PATH);
-  // }
+  if (isMember) {
+    return redirect(SIGN_IN_PATH);
+  }
 
   return (
     <main className="px-8 py-4">
