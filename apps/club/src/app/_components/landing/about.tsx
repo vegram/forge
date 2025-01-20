8d84e2af323b59e958182297011b1661e2b0bc67
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2025-01-20 12:30:55 -0500

    remove opacity

--- apps/club/src/app/_components/landing/about.tsx
+++ apps/club/src/app/_components/landing/about.tsx
@@ -1,10 +1,10 @@
 export default function About() {
   return (
     <div className="m-2 flex h-screen flex-col items-center justify-center">
-      <p className="font-pragati text-center text-[30px] font-bold leading-[102px] tracking-[0.05em] text-white opacity-0 [text-shadow:0px_0px_281.064px_#6B21A8,0px_0px_160.608px_#6B21A8,0px_0px_93.688px_#6B21A8,0px_0px_46.844px_#6B21A8,0px_0px_13.384px_#6B21A8,0px_0px_6.692px_#6B21A8] md:text-[60px]">
+      <p className="font-pragati text-center text-[30px] font-bold leading-[102px] tracking-[0.05em] text-white [text-shadow:0px_0px_281.064px_#6B21A8,0px_0px_160.608px_#6B21A8,0px_0px_93.688px_#6B21A8,0px_0px_46.844px_#6B21A8,0px_0px_13.384px_#6B21A8,0px_0px_6.692px_#6B21A8] md:text-[60px]">
         ABOUT US
       </p>
-      <p className="font-poppins w-full max-w-[1276px] transform px-4 text-center text-[16px] font-semibold leading-[24px] tracking-[0.05em] text-white opacity-0 [animation-delay:0.7s] md:text-[36px] md:leading-[54px]">
+      <p className="font-poppins w-full max-w-[1276px] transform px-4 text-center text-[16px] font-semibold leading-[24px] tracking-[0.05em] text-white md:text-[36px] md:leading-[54px]">
         The FitnessGram™ Pacer Test is a multistage aerobic capacity test that
         progressively gets more difficult as it continues. The 20 meter pacer
         test will begin in 30 seconds. Line up at the start. The running speed
