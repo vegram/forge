2b0b432ac243df9cc67ab6954d1cd0c0833d676c
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2025-01-27 10:09:14 -0500

    Sponsor mobile optimization

--- apps/club/src/app/_components/landing/sponsors-assets/sponsor-card.tsx
+++ apps/club/src/app/_components/landing/sponsors-assets/sponsor-card.tsx
@@ -24,7 +24,7 @@ export default function SponsorCard({
         padding: "3px",
       }}
     >
-      <div className="relative flex h-56 w-48 flex-col justify-between rounded-[calc(1.5rem-3px)] bg-blue-950 p-4 transition-all duration-300 group-hover:bg-blue-900">
+      <div className="relative flex h-44 w-28 flex-col justify-between rounded-[calc(1.5rem-3px)] bg-blue-950 p-4 transition-all duration-300 group-hover:bg-blue-900 md:h-56 md:w-48">
         <div className="absolute inset-0 bg-gradient-to-br from-purple-900/20 via-blue-950/30 to-blue-950/40 opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
 
         <div className="relative z-10 mb-2 text-lg font-bold text-gray-100 transition-colors duration-300 group-hover:bg-gradient-to-r group-hover:from-purple-400 group-hover:to-pink-600 group-hover:bg-clip-text group-hover:text-transparent">
